31f37ccf94abe0bfba0ca85729e673914a20f160
TreyTheLooneyCoder <roytreslll@gmail.com>
2024-12-03 14:44:31 -0800

    set up the js id's

---
+++ scripts/app.js
@@ -0,0 +1,17 @@
+let workTimeCurrent = document.getElementById('workTimeCurrent');
+let workTimePrevious = document.getElementById('workTimePrevious');
+
+let playTimeCurrent = document.getElementById('playTimeCurrent');
+let playTimePrevious = document.getElementById('playTimePrevious');
+
+let studyTimeCurrent = document.getElementById('studyTimeCurrent');
+let studyTimePrevious = document.getElementById('studyTimePrevious');
+
+let ExerciseTimeCurrent = document.getElementById('ExerciseTimeCurrent');
+let ExerciseTimePrevious = document.getElementById('ExerciseTimePrevious');
+
+let socialTimeCurrent = document.getElementById('socialTimeCurrent');
+let socialTimePrevious = document.getElementById('socialTimePrevious');
+
+let selfCareTimeCurrent = document.getElementById('selfCareTimeCurrent');
+let selfCareTimePrevious = document.getElementById('selfCareTimePrevious');
