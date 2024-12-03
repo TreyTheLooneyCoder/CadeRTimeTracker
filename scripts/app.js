82278593f21a7ebd42ca2c3b297f62c13af8e73e
TreyTheLooneyCoder <roytreslll@gmail.com>
2024-12-03 15:38:08 -0800

    -Thinking of a clever way to populate all of this data in the write spots.
    -Need to take a walk.

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,14 @@
-// I need a variable to store previous and current times 
-// I need a varable and functions for storing the monthly, daily and weekly hours seperately 
+// I need a variable to store previous and current times, as well as the sections themselves 
+let currentTime;
+let previousTimes;
+
+let workHrs = document.getElementById('workHrs');
+let playHrs = document.getElementById('playHrs');
+let studyHrs = document.getElementById('studyHrs');
+let exerciseHrs = document.getElementById('exerciseHrs');
+let socialHrs = document.getElementById('socialHrs');
+let selfCareHrs = document.getElementById('selfCareHrs');
+// I need a varable and functions for storing the monthly, daily and weekly hours seperately
 // I need a function to switch the times shown on the page 
 
 
@@ -12,3 +21,7 @@ function GetHours(){
 }
 
 GetHours()
+
+function dailyHours(){
+    // currentTime = hours.
+}
